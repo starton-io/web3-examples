496f15ca46bf8b850448c9a5caf062e96c3143cb
Calixte DE TOURRIS <cdetourrispro@gmail.com>
2023-10-24 17:33:03 +0200

    chores: cleaned the code

--- playground/mint-your-first-nft/src/mint-nft.ts
+++ playground/mint-your-first-nft/src/mint-nft.ts
@@ -1,11 +1,5 @@
 import { starton } from './index'
-import { AxiosError } from 'axios'
 
-/*
-|--------------------------------------------------------------------------
-| Contract
-|--------------------------------------------------------------------------
-*/
 export interface MintNftProps {
   network: string
   smartContractAddress: string
@@ -13,26 +7,12 @@ export interface MintNftProps {
   signerWallet: string
 }
 
-/*
-|--------------------------------------------------------------------------
-| Method
-|--------------------------------------------------------------------------
-*/
 export async function mintNft ({ network, cid, smartContractAddress, signerWallet }: MintNftProps) {
-  try {
-    const response = await starton.post(`/smart-contract/${network}/${smartContractAddress}/call`,
-      {
-        functionName: 'mint(address,string)',
-        params: [smartContractAddress, cid],
-        signerWallet,
-        speed: 'average'
-      })
-    return response.data
-  } catch (error) {
-    if (error instanceof AxiosError) {
-      console.log(error.response?.data)
-    } else {
-      console.log(error)
-    }
-  }
+  return await starton.post(`/smart-contract/${network}/${smartContractAddress}/call`,
+    {
+      functionName: 'mint(address,string)',
+      params: [smartContractAddress, cid],
+      signerWallet,
+      speed: 'average'
+    })
 }
